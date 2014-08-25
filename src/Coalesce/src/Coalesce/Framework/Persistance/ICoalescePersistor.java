d98fa8e602bb82eec55fc96e2df9d0431c89a549
Derek Clemenzi <dclemenzi@incadencecorp.com>
2014-08-25 15:36:46 -0400

    Added missing functions and removed CallResults

--- src/Coalesce/src/Coalesce/Framework/Persistance/ICoalescePersistor.java
+++ src/Coalesce/src/Coalesce/Framework/Persistance/ICoalescePersistor.java
@@ -1,23 +1,52 @@
 package Coalesce.Framework.Persistance;
 
+import java.util.List;
+
+import org.joda.time.DateTime;
+
 import Coalesce.Framework.GeneratedJAXB.*;
 import unity.core.runtime.CallResult;
 
 public interface ICoalescePersistor {
+		
+	public class EntityMetaData {
+
+		public String Id;
+		public String Type;
+		public String Key;
+		
+	}
 	
-	public CallResult Initialize(ICoalesceEncrypter Encrypter);
+	public boolean Initialize(ICoalesceEncrypter Encrypter);
 	
-	public CallResult SetEntity(Entity entity);
+	public boolean SetEntity(Entity entity);
 	
-	public CallResult GetEntity(String Key, Entity entity);
-	public CallResult GetEntity(String EntityId, String EntityIdType, Entity entity);
-	public CallResult GetEntity(String Name, String EntityId, String EntityIdType, Entity entity);
+	// Get Entity
+	public Entity GetEntity(String Key);
+	public Entity GetEntity(String EntityId, String EntityIdType);
+	public Entity GetEntity(String Name, String EntityId, String EntityIdType);
 	
-	public CallResult GetEntityXml(String Key, String Xml);
-	public CallResult GetEntityXml(String EntityId, String EntityIdType, String Xml);
-	public CallResult GetEntityXml(String Name, String EntityId, String EntityIdType, String Xml);
+	// Get Entity XML
+	public String GetEntityXml(String Key);
+	public String GetEntityXml(String EntityId, String EntityIdType);
+	public String GetEntityXml(String Name, String EntityId, String EntityIdType);
 	
-	public CallResult getFieldValue(String fieldKey, String value);
-	public CallResult getXPath(String Key, String ObjectType, String EntityKey, String XPath);
+	public Object GetFieldValue(String fieldKey);
+	public String GetXPath(String Key, String ObjectType, String EntityKey);
+	
+	//Deprecated Functions
+	public CallResult PersistEntityTemplate(/*CoalesceEntityTemplate EntityTemplate*/);
+	
+	// Entity Templates
+	public String GetEntityTemplateXml(String Key);
+	public String GetEntityTemplateXml(String Name, String Source, String Version);
+	public String GetEntityTemplateKey(String Name, String Source, String Version);
+	public String GetEntityTemplateMetadata();
+		
+	public DateTime GetCoalesceDataObjectLastModified(String Key, String ObjectType);
+	public List<String> GetCoalesceEntityKeysForEntityId(String EntityId, String EntityIdType, String EntityName, String EntitySource);
+	public EntityMetaData GetCoalesceEntityIdAndTypeForKey(String Key);
+	public byte[] GetBinaryArray(String BinaryFieldKey);
+
 	
 }
