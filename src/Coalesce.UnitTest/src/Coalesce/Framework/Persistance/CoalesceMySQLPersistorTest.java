1a0fa0fa8320f33e48200f7b93b7d3eaa6a5856a
Derek Clemenzi <dclemenzi@incadencecorp.com>
2014-09-02 10:24:46 -0400

    Updated Unit Test to create new entities.

--- src/Coalesce.UnitTest/src/Coalesce/Framework/Persistance/CoalesceMySQLPersistorTest.java
+++ src/Coalesce.UnitTest/src/Coalesce/Framework/Persistance/CoalesceMySQLPersistorTest.java
@@ -7,91 +7,145 @@ import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import Coalesce.Common.UnitTest.CoalesceTypeInstances;
 import Coalesce.Framework.CoalesceFramework;
+import Coalesce.Framework.DataModel.ECoalesceFieldDataTypes;
 import Coalesce.Framework.DataModel.XsdEntity;
+import Coalesce.Framework.DataModel.XsdFieldDefinition;
+import Coalesce.Framework.DataModel.XsdLinkageSection;
+import Coalesce.Framework.DataModel.XsdRecord;
+import Coalesce.Framework.DataModel.XsdRecordset;
+import Coalesce.Framework.DataModel.XsdSection;
 
 import com.database.persister.MySQLPersistor;
 import com.database.persister.ServerConn;
 
 /*-----------------------------------------------------------------------------'
-Copyright 2014 - InCadence Strategic Solutions Inc., All Rights Reserved
+ Copyright 2014 - InCadence Strategic Solutions Inc., All Rights Reserved
 
-Notwithstanding any contractor copyright notice, the Government has Unlimited
-Rights in this work as defined by DFARS 252.227-7013 and 252.227-7014.  Use
-of this work other than as specifically authorized by these DFARS Clauses may
-violate Government rights in this work.
+ Notwithstanding any contractor copyright notice, the Government has Unlimited
+ Rights in this work as defined by DFARS 252.227-7013 and 252.227-7014.  Use
+ of this work other than as specifically authorized by these DFARS Clauses may
+ violate Government rights in this work.
 
-DFARS Clause reference: 252.227-7013 (a)(16) and 252.227-7014 (a)(16)
-Unlimited Rights. The Government has the right to use, modify, reproduce,
-perform, display, release or disclose this computer software and to have or
-authorize others to do so.
+ DFARS Clause reference: 252.227-7013 (a)(16) and 252.227-7014 (a)(16)
+ Unlimited Rights. The Government has the right to use, modify, reproduce,
+ perform, display, release or disclose this computer software and to have or
+ authorize others to do so.
 
-Distribution Statement D. Distribution authorized to the Department of
-Defense and U.S. DoD contractors only in support of U.S. DoD efforts.
------------------------------------------------------------------------------*/
+ Distribution Statement D. Distribution authorized to the Department of
+ Defense and U.S. DoD contractors only in support of U.S. DoD efforts.
+ -----------------------------------------------------------------------------*/
 
 public class CoalesceMySQLPersistorTest {
-	static ServerConn serCon;
-	
+
+    static ServerConn serCon;
+    static MySQLPersistor mySQLPersistor;
     private static CoalesceFramework _coalesceFramework;
-    
-	@BeforeClass
-	public static void Initialize() {
-		serCon = new ServerConn();
-		serCon.setURL("jdbc:mysql://localhost:3306/coalescedatabase");
-		serCon.setUser("root");
-		serCon.setPassword("web23ler");
 
-	    MySQLPersistor mySQLPersistor = new MySQLPersistor();
-	    
-	    mySQLPersistor.Initialize(serCon);
-	    CoalesceMySQLPersistorTest._coalesceFramework = new CoalesceFramework();
-	    CoalesceMySQLPersistorTest._coalesceFramework.Initialize(mySQLPersistor);
-	    
-	}
-	
-	@Test
-	public void TestConnection() {
-		
-		fail("Not yet implemented");
-	}
+    private static XsdEntity _entity;
+    private static String _fieldKey;
 
-	@Test
-	public void TestSaveEntity() {
-	    
-	    XsdEntity mission = new XsdEntity(); 
-	    mission.Initialize(CoalesceTypeInstances.TESTMISSION);
-	    	    
-	    assertTrue(CoalesceMySQLPersistorTest._coalesceFramework.SaveCoalesceEntity(mission));
-	    
-	}
-	
-	@Test
-	public void TestGetEntity() {
-	    XsdEntity ent = new XsdEntity();
-	    ent=CoalesceMySQLPersistorTest._coalesceFramework.GetCoalesceEntity("62857EF8-3930-4F0E-BAE3-093344EBF389");
-	}
+    @BeforeClass
+    public static void Initialize()
+    {
 
-	@Test
-	public void TestGetFieldValue() {
-	    assertFalse(CoalesceMySQLPersistorTest._coalesceFramework.GetCoalesceFieldValue("02e03da7-de93-4deb-8c7c-be65d25f918e"));
-	}
-	
-    private boolean assertFalse(String object) {
-		if(object!=null)
-			return true;
-		return false;
-	}
+        serCon = new ServerConn();
+        serCon.setURL("jdbc:mysql://localhost:3306/coalescedatabase");
+        serCon.setUser("root");
+        serCon.setPassword("Passw0rd");
 
-	@Test
-	public void TestGetXPath() {
-		fail("Not yet implemented");
-	}
+        mySQLPersistor = new MySQLPersistor();
+        mySQLPersistor.Initialize(serCon);
 
-	@After
-	public void Finalize() {
-		
-	}
-	
+        mySQLPersistor.Initialize(serCon);
+        CoalesceMySQLPersistorTest._coalesceFramework = new CoalesceFramework();
+        CoalesceMySQLPersistorTest._coalesceFramework.Initialize(mySQLPersistor);
+
+        // Create Test Entity
+        _entity = new XsdEntity();
+
+        XsdSection section = null;
+        XsdRecordset recordSet = null;
+        XsdRecord record = null;
+
+        // Create Entity
+        _entity = XsdEntity.Create("TestEntity", "Unit Test", "1.0.0.0", "", "", "");
+
+        XsdLinkageSection.Create(_entity, true);
+
+        section = XsdSection.Create(_entity, "Live Status Section", true);
+        recordSet = XsdRecordset.Create(section, "Live Status Recordset");
+        XsdFieldDefinition.Create(recordSet, "CurrentStatus", ECoalesceFieldDataTypes.StringType);
+
+        record = recordSet.AddNew();
+        record.SetFieldValue("CurrentStatus", "Test Status");
+
+        _fieldKey = record.GetFieldByName("CurrentStatus").GetKey();
+
+    }
+
+    @Test
+    public void TestConnection()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void TestSaveEntity()
+    {
+        try
+        {
+            assertTrue(CoalesceMySQLPersistorTest._coalesceFramework.SaveCoalesceEntity(_entity));
+        }
+        catch (Exception ex)
+        {
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void TestGetEntity()
+    {
+        try
+        {
+            XsdEntity ent = new XsdEntity();
+            ent = CoalesceMySQLPersistorTest._coalesceFramework.GetCoalesceEntity(_entity.GetKey());
+
+            assertTrue(ent != null);
+        }
+        catch (Exception ex)
+        {
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void TestGetFieldValue()
+    {
+
+        try
+        {
+            String fieldValue = CoalesceMySQLPersistorTest._coalesceFramework.GetCoalesceFieldValue(_fieldKey);
+
+            assertTrue(fieldValue.equals("Test Status"));
+        }
+        catch (Exception ex)
+        {
+            fail(ex.getMessage());
+        }
+
+    }
+
+    @Test
+    public void TestGetXPath()
+    {
+        fail("Not yet implemented");
+    }
+
+    @After
+    public void Finalize()
+    {
+
+    }
+
 }
