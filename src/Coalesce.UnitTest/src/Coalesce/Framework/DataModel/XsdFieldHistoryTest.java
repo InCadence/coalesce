72e3870d69999dc92291544d97f33b43d92469a0
wbrannoc <william.brannock@gmail.com>
2014-09-03 14:54:35 -0400

    Added missing copyright headers

--- src/Coalesce.UnitTest/src/Coalesce/Framework/DataModel/XsdFieldHistoryTest.java
+++ src/Coalesce.UnitTest/src/Coalesce/Framework/DataModel/XsdFieldHistoryTest.java
@@ -1,9 +1,7 @@
 package Coalesce.Framework.DataModel;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.UnsupportedEncodingException;
@@ -25,6 +23,23 @@ import Coalesce.Framework.GeneratedJAXB.Entity.Section.Recordset;
 import Coalesce.Framework.GeneratedJAXB.Entity.Section.Recordset.Record;
 import Coalesce.Framework.GeneratedJAXB.Entity.Section.Recordset.Record.Field;
 import Coalesce.Framework.GeneratedJAXB.Entity.Section.Recordset.Record.Field.Fieldhistory;
+
+/*-----------------------------------------------------------------------------'
+Copyright 2014 - InCadence Strategic Solutions Inc., All Rights Reserved
+
+Notwithstanding any contractor copyright notice, the Government has Unlimited
+Rights in this work as defined by DFARS 252.227-7013 and 252.227-7014.  Use
+of this work other than as specifically authorized by these DFARS Clauses may
+violate Government rights in this work.
+
+DFARS Clause reference: 252.227-7013 (a)(16) and 252.227-7014 (a)(16)
+Unlimited Rights. The Government has the right to use, modify, reproduce,
+perform, display, release or disclose this computer software and to have or
+authorize others to do so.
+
+Distribution Statement D. Distribution authorized to the Department of
+Defense and U.S. DoD contractors only in support of U.S. DoD efforts.
+-----------------------------------------------------------------------------*/
 
 public class XsdFieldHistoryTest {
 
@@ -919,7 +934,7 @@ public class XsdFieldHistoryTest {
         field.SetTypedValue(JodaDateTimeHelper.NowInUtc());
         
         XsdFieldHistory fh = field.GetHistory().get(0);
-        field.SetTypedValue(UUID.randomUUID());
+        fh.SetTypedValue(UUID.randomUUID());
 
     }
 
