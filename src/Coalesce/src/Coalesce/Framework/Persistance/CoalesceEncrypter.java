313a7a3ab3d64682398c7f8aa7ef1c2a6b866db0
Derek Clemenzi <dclemenzi@incadencecorp.com>
2014-09-09 10:27:00 -0400

    Applied Formatting Refactored function names to lowerCamelCase

--- src/Coalesce/src/Coalesce/Framework/Persistance/CoalesceEncrypter.java
+++ src/Coalesce/src/Coalesce/Framework/Persistance/CoalesceEncrypter.java
@@ -21,188 +21,223 @@ import unity.core.runtime.CallResult.CallResults;
 
 public abstract class CoalesceEncrypter implements ICoalesceEncrypter {
 
-	private static HashMap<String, EncoderParameters> _parameters = new HashMap<String, EncoderParameters>();
+    private static HashMap<String, EncoderParameters> _parameters = new HashMap<String, EncoderParameters>();
 
-	private EncoderParameters _parameter;
-	private String _Transformation;
-	
-	public CoalesceEncrypter(String passPhrase) throws InvalidKeyException, NoSuchAlgorithmException, UnsupportedEncodingException {
-		
-		this._Transformation = "AES/CBC/PKCS5Padding";
-		
-	    synchronized (_parameters) {
-	    	_parameter = _parameters.get(passPhrase);
-	        if (_parameter == null) {
-	           _parameter = new EncoderParameters(passPhrase);
-	           _parameters.put(passPhrase, _parameter);
-	        }
-	     }
-	      
-	}
-	
-	public Cipher GetEncryptionCipher() {
-		
+    private EncoderParameters _parameter;
+    private String _Transformation;
+
+    public CoalesceEncrypter(String passPhrase) throws InvalidKeyException, NoSuchAlgorithmException,
+            UnsupportedEncodingException
+    {
+
+        this._Transformation = "AES/CBC/PKCS5Padding";
+
+        synchronized (_parameters)
+        {
+            _parameter = _parameters.get(passPhrase);
+            if (_parameter == null)
+            {
+                _parameter = new EncoderParameters(passPhrase);
+                _parameters.put(passPhrase, _parameter);
+            }
+        }
+
+    }
+
+    public Cipher getEncryptionCipher()
+    {
+
         Cipher cipher = null;
-        
-		try {
-			
-			// Load Cipher
-			cipher = Cipher.getInstance(this._Transformation);
-			
-	        // Initialize Cipher
-			cipher.init(Cipher.ENCRYPT_MODE, _parameter.key, new IvParameterSpec(_parameter.iv));
-			
-		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e) {
-			CallResult.log(CallResults.FAILED_ERROR, e, this);
-			cipher = null;
-		}
-		
-		return cipher; 
-		
-	}
-	
-	public Cipher GetDecryptionCipher() {
-		
+
+        try
+        {
+
+            // Load Cipher
+            cipher = Cipher.getInstance(this._Transformation);
+
+            // Initialize Cipher
+            cipher.init(Cipher.ENCRYPT_MODE, _parameter.key, new IvParameterSpec(_parameter.iv));
+
+        }
+        catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e)
+        {
+            CallResult.log(CallResults.FAILED_ERROR, e, this);
+            cipher = null;
+        }
+
+        return cipher;
+
+    }
+
+    public Cipher getDecryptionCipher()
+    {
+
         Cipher cipher = null;
-        
-		try {
-			
-			// Load Cipher
-			cipher = Cipher.getInstance(this._Transformation);
-			
-	        // Initialize Cipher
-			cipher.init(Cipher.DECRYPT_MODE, _parameter.key, new IvParameterSpec(_parameter.iv));
-			
-		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e) {
-			CallResult.log(CallResults.FAILED_ERROR, e, this);
-			cipher = null;
-		}
-		
-		return cipher; 
-		
-	}
-	
-	@Override
-	public String DecryptEntity(byte[] EntityEncryptedBytes) {
-		return this.DecryptValue(EntityEncryptedBytes);
-	}
 
-	@Override
-	public String DecryptEntity(String EntityEncryptedBase64) {
-		return this.DecryptValue(EntityEncryptedBase64);
-	}
+        try
+        {
 
-	@Override
-	public byte[] EncryptEntity(String EntityXml) {
-		return this.EncryptValue(EntityXml);
-	}
+            // Load Cipher
+            cipher = Cipher.getInstance(this._Transformation);
 
-	@Override
-	public String EncryptEntityToBase64(String EntityXml) {
-		return this.EncryptValueToBase64(EntityXml);
-	}
+            // Initialize Cipher
+            cipher.init(Cipher.DECRYPT_MODE, _parameter.key, new IvParameterSpec(_parameter.iv));
 
-	@Override
-	public String DecryptValue(byte[] ValueEncryptedBytes) {
-		
-		String DecryptString = null;
+        }
+        catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e)
+        {
+            CallResult.log(CallResults.FAILED_ERROR, e, this);
+            cipher = null;
+        }
 
-		byte[] utf8 = this.DecryptValueToBytes(ValueEncryptedBytes);
-		
-	    try {
+        return cipher;
 
-		    DecryptString = new String(utf8, "UTF8");
-			
-		} catch (UnsupportedEncodingException e) {
-			CallResult.log(CallResults.FAILED_ERROR, e, this);
-		}
-	    
-	    return DecryptString;
-	    
-	}
+    }
 
-	@Override
-	public String DecryptValue(String ValueEncryptedBase64) {
-		
-		Base64 Encoder = new Base64();
-		
-		byte[] DecodedBytes = Encoder.decode(ValueEncryptedBase64);
-		
-	    return this.DecryptValue(DecodedBytes);
-	    
-	}
+    @Override
+    public String decryptEntity(byte[] EntityEncryptedBytes)
+    {
+        return this.decryptValue(EntityEncryptedBytes);
+    }
 
-	@Override
-	public byte[] DecryptValueToBytes(byte[] ValueEncryptedBytes) {
-		
-	    byte[] utf8 = null;
+    @Override
+    public String decryptEntity(String EntityEncryptedBase64)
+    {
+        return this.decryptValue(EntityEncryptedBase64);
+    }
 
-		// Decrypt Data
-	    try {
-		    utf8 = this.GetDecryptionCipher().doFinal(ValueEncryptedBytes);
-		} catch (IllegalBlockSizeException | BadPaddingException e) {
-			CallResult.log(CallResults.FAILED_ERROR, e, this);
-		}
-	    
-	    return utf8;
-	    
-	}
+    @Override
+    public byte[] encryptEntity(String EntityXml)
+    {
+        return this.encryptValue(EntityXml);
+    }
 
-	@Override
-	public byte[] EncryptValue(String Value) {
-		return this.EncryptValue(Value.getBytes());
-	}
+    @Override
+    public String encryptEntityToBase64(String EntityXml)
+    {
+        return this.encryptValueToBase64(EntityXml);
+    }
 
-	@Override
-	public String EncryptValueToBase64(String Value) {
-		
-		byte[] utf8 = this.EncryptValue(Value);
-		
-		if (utf8 != null) {
+    @Override
+    public String decryptValue(byte[] ValueEncryptedBytes)
+    {
 
-			Base64 Encoder = new Base64();
-			return Encoder.encodeToString(utf8);
-			
-		} 
-		else {
-			return null;
-		}
-		
-	}
+        String DecryptString = null;
 
-	@Override
-	public byte[] EncryptValue(byte[] ValueBytes) {
-		
-		byte[] utf8 = null;
-		
-		try {
-			utf8 = this.GetEncryptionCipher().doFinal(ValueBytes);
-		} catch (IllegalBlockSizeException | BadPaddingException e) {
-			CallResult.log(CallResults.FAILED_ERROR, e, this);
-		}
-		
-		return utf8;
-	}
+        byte[] utf8 = this.decryptValueToBytes(ValueEncryptedBytes);
 
-	private class EncoderParameters {
-		
-		public final SecretKey key;
-		public final byte[] iv;
-		      
-		public EncoderParameters(String passPhrase) throws InvalidKeyException, NoSuchAlgorithmException, UnsupportedEncodingException {
-		         
-			// Salt
-			byte[] salt = {0x0, 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, (byte) 0xF1, (byte) 0xF0, (byte) 0xEE, 0x21, 0x22, 0x45};
+        try
+        {
 
-		    Rfc2898DeriveBytes derived_bytes = new Rfc2898DeriveBytes(passPhrase, salt, 1000);
-		         
-		    // Create Key and IV
-		    byte[] key_bytes = derived_bytes.getBytes(32);
-		    iv = derived_bytes.getBytes(16);
+            DecryptString = new String(utf8, "UTF8");
 
-		    key = new SecretKeySpec(key_bytes, "AES");
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            CallResult.log(CallResults.FAILED_ERROR, e, this);
+        }
 
-		}
-	}
-	
+        return DecryptString;
+
+    }
+
+    @Override
+    public String decryptValue(String ValueEncryptedBase64)
+    {
+
+        Base64 Encoder = new Base64();
+
+        byte[] DecodedBytes = Encoder.decode(ValueEncryptedBase64);
+
+        return this.decryptValue(DecodedBytes);
+
+    }
+
+    @Override
+    public byte[] decryptValueToBytes(byte[] ValueEncryptedBytes)
+    {
+
+        byte[] utf8 = null;
+
+        // Decrypt Data
+        try
+        {
+            utf8 = this.getDecryptionCipher().doFinal(ValueEncryptedBytes);
+        }
+        catch (IllegalBlockSizeException | BadPaddingException e)
+        {
+            CallResult.log(CallResults.FAILED_ERROR, e, this);
+        }
+
+        return utf8;
+
+    }
+
+    @Override
+    public byte[] encryptValue(String Value)
+    {
+        return this.encryptValue(Value.getBytes());
+    }
+
+    @Override
+    public String encryptValueToBase64(String Value)
+    {
+
+        byte[] utf8 = this.encryptValue(Value);
+
+        if (utf8 != null)
+        {
+
+            Base64 Encoder = new Base64();
+            return Encoder.encodeToString(utf8);
+
+        }
+        else
+        {
+            return null;
+        }
+
+    }
+
+    @Override
+    public byte[] encryptValue(byte[] ValueBytes)
+    {
+
+        byte[] utf8 = null;
+
+        try
+        {
+            utf8 = this.getEncryptionCipher().doFinal(ValueBytes);
+        }
+        catch (IllegalBlockSizeException | BadPaddingException e)
+        {
+            CallResult.log(CallResults.FAILED_ERROR, e, this);
+        }
+
+        return utf8;
+    }
+
+    private class EncoderParameters {
+
+        public final SecretKey key;
+        public final byte[] iv;
+
+        public EncoderParameters(String passPhrase) throws InvalidKeyException, NoSuchAlgorithmException,
+                UnsupportedEncodingException
+        {
+
+            // Salt
+            byte[] salt = { 0x0, 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, (byte) 0xF1, (byte) 0xF0, (byte) 0xEE, 0x21, 0x22, 0x45 };
+
+            Rfc2898DeriveBytes derived_bytes = new Rfc2898DeriveBytes(passPhrase, salt, 1000);
+
+            // Create Key and IV
+            byte[] key_bytes = derived_bytes.getBytes(32);
+            iv = derived_bytes.getBytes(16);
+
+            key = new SecretKeySpec(key_bytes, "AES");
+
+        }
+    }
+
 }
--- src/Coalesce/src/Coalesce/Framework/Persistance/ICoalesceEncrypter.java
+++ src/Coalesce/src/Coalesce/Framework/Persistance/ICoalesceEncrypter.java
@@ -1,36 +1,42 @@
 package Coalesce.Framework.Persistance;
 
 /*-----------------------------------------------------------------------------'
-Copyright 2014 - InCadence Strategic Solutions Inc., All Rights Reserved
+ Copyright 2014 - InCadence Strategic Solutions Inc., All Rights Reserved
 
-Notwithstanding any contractor copyright notice, the Government has Unlimited
-Rights in this work as defined by DFARS 252.227-7013 and 252.227-7014.  Use
-of this work other than as specifically authorized by these DFARS Clauses may
-violate Government rights in this work.
+ Notwithstanding any contractor copyright notice, the Government has Unlimited
+ Rights in this work as defined by DFARS 252.227-7013 and 252.227-7014.  Use
+ of this work other than as specifically authorized by these DFARS Clauses may
+ violate Government rights in this work.
 
-DFARS Clause reference: 252.227-7013 (a)(16) and 252.227-7014 (a)(16)
-Unlimited Rights. The Government has the right to use, modify, reproduce,
-perform, display, release or disclose this computer software and to have or
-authorize others to do so.
+ DFARS Clause reference: 252.227-7013 (a)(16) and 252.227-7014 (a)(16)
+ Unlimited Rights. The Government has the right to use, modify, reproduce,
+ perform, display, release or disclose this computer software and to have or
+ authorize others to do so.
 
-Distribution Statement D. Distribution authorized to the Department of
-Defense and U.S. DoD contractors only in support of U.S. DoD efforts.
------------------------------------------------------------------------------*/
+ Distribution Statement D. Distribution authorized to the Department of
+ Defense and U.S. DoD contractors only in support of U.S. DoD efforts.
+ -----------------------------------------------------------------------------*/
 
 public interface ICoalesceEncrypter {
 
-	public String DecryptEntity(byte[] EntityEncryptedBytes);
-	public String DecryptEntity(String EntityEncryptedBase64);
+    public String decryptEntity(byte[] EntityEncryptedBytes);
 
-	public byte[] EncryptEntity(String EntityXml);
-	public String EncryptEntityToBase64(String EntityXml);
+    public String decryptEntity(String EntityEncryptedBase64);
 
-	public String DecryptValue(byte[] ValueEncryptedBytes);
-	public String DecryptValue(String ValueEncryptedBase64);
-	public byte[] DecryptValueToBytes(byte[] ValueEncryptedBytes);
+    public byte[] encryptEntity(String EntityXml);
 
-	public byte[] EncryptValue(String Value);
-	public String EncryptValueToBase64(String Value);
-	public byte[] EncryptValue(byte[] ValueBytes);
-	
+    public String encryptEntityToBase64(String EntityXml);
+
+    public String decryptValue(byte[] ValueEncryptedBytes);
+
+    public String decryptValue(String ValueEncryptedBase64);
+
+    public byte[] decryptValueToBytes(byte[] ValueEncryptedBytes);
+
+    public byte[] encryptValue(String Value);
+
+    public String encryptValueToBase64(String Value);
+
+    public byte[] encryptValue(byte[] ValueBytes);
+
 }
