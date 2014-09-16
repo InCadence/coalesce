655059b63e1be5a6892743b2df1d55c614337dea
Jeff Hamlin <jeffrey.hamlin@gmail.com>
2014-09-16 15:54:08 -0400

    Maven updates, and import fixes so Derek doesn't kill me

--- src/Coalesce.Persister/src/com/database/persister/PostGresDataConnector.java
+++ src/Coalesce.Persister/src/com/database/persister/PostGresDataConnector.java
@@ -5,7 +5,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Properties;
 
 import Coalesce.Common.Exceptions.CoalescePersistorException;
 
