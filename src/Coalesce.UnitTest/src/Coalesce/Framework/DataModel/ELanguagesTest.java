68a80195b327ae7a2f74d4c98e5df7f13da341d9
kwimberly <kwimberly@incadencecorp.com>
2014-09-19 16:30:25 -0400

    Updated test to reflect changes. Test complete.

--- src/Coalesce.UnitTest/src/Coalesce/Framework/DataModel/ELanguagesTest.java
+++ src/Coalesce.UnitTest/src/Coalesce/Framework/DataModel/ELanguagesTest.java
@@ -22,171 +22,140 @@ public class ELanguagesTest {
     @Test
     public void GetValueTest()
     {
-
-        // String a = "HasParticipant";
-        // ELinkTypes value = ELinkTypes.HasParticipant;
-
-        // assertEquals(a, value.getLabel());
-
+        int language = 0;
         for (int i = 0; i < 1020; i++)
         {
 
-            if (i == 0)
-            {
-                assertEquals(i, ELanguages.English);
+            switch (language) {
+            case 1:
+                assertEquals(language, ELanguages.English.getValue());
+                break;
+
+            case 2:
+                assertEquals(language, ELanguages.French.getValue());
+                break;
+
+            case 3:
+                assertEquals(language, ELanguages.Arabic.getValue());
+                break;
+
+            case 4:
+                assertEquals(language, ELanguages.Spanish.getValue());
+                break;
+
+            case 5:
+                assertEquals(language, ELanguages.Chinese.getValue());
+                break;
+
+            case 6:
+                assertEquals(language, ELanguages.Russian.getValue());
+                break;
+
+            case 7:
+                assertEquals(language, ELanguages.German.getValue());
+                break;
+
+            case 8:
+                assertEquals(language, ELanguages.Korean.getValue());
+                break;
+
+            case 9:
+                assertEquals(language, ELanguages.Japanese.getValue());
+                break;
+
+            case 10:
+                assertEquals(language, ELanguages.Greek.getValue());
+                break;
+
+            case 11:
+                assertEquals(language, ELanguages.Italian.getValue());
+                break;
+
+            case 12:
+                assertEquals(language, ELanguages.Portuguese.getValue());
+                break;
+
+            case 13:
+                assertEquals(language, ELanguages.Swedish.getValue());
+                break;
+
+            case 14:
+                assertEquals(language, ELanguages.Romanian.getValue());
+                break;
+
+            case 15:
+                assertEquals(language, ELanguages.Norwegian.getValue());
+                break;
+
+            case 16:
+                assertEquals(language, ELanguages.Lithuanian.getValue());
+                break;
+
+            case 17:
+                assertEquals(language, ELanguages.Hungarian.getValue());
+                break;
+
+            case 18:
+                assertEquals(language, ELanguages.Polish.getValue());
+                break;
+
+            case 19:
+                assertEquals(language, ELanguages.Dutch.getValue());
+                break;
+
+            case 20:
+                assertEquals(language, ELanguages.Danish.getValue());
+                break;
+
+            case 21:
+                assertEquals(language, ELanguages.Pashtu.getValue());
+                break;
+
+            case 22:
+                assertEquals(language, ELanguages.HaitianCreole.getValue());
+                break;
+
+            case 23:
+                assertEquals(language, ELanguages.Dari.getValue());
+                break;
+
+            case 24:
+                assertEquals(language, ELanguages.Custom1.getValue());
+                break;
+
+            case 25:
+                assertEquals(language, ELanguages.Custom2.getValue());
+                break;
+
+            case 26:
+                assertEquals(language, ELanguages.Custom3.getValue());
+                break;
+
+            case 27:
+                assertEquals(language, ELanguages.Custom4.getValue());
+                break;
+
+            case 28:
+                assertEquals(language, ELanguages.Custom5.getValue());
+                break;
+
+            case 29:
+                assertEquals(language, ELanguages.Custom6.getValue());
+                break;
+
+            case 30:
+                assertEquals(language, ELanguages.Custom7.getValue());
+                break;
+
+            case 31:
+                assertEquals(language, ELanguages.Custom8.getValue());
+                break;
             }
-            
-            else if (i == 1)
-            {
-                assertEquals(i, ELanguages.French);
-            }
-            
-            else if (i == 2)
-            {
-                assertEquals(i, ELanguages.Arabic);
-            }
-            
-            else if (i == 3)
-            {
-                assertEquals(i, ELanguages.Spanish);
-            }
-            
-            else if (i == 4)
-            {
-                assertEquals(i, ELanguages.Chinese);
-            }
-            
-            else if (i == 5)
-            {
-                assertEquals(i, ELanguages.Russian);
-            }
-            
-            else if (i == 6)
-            {
-                assertEquals(i, ELanguages.German);
-            }
-            
-            else if (i == 7)
-            {
-                assertEquals(i, ELanguages.Korean);
-            }
-            
-            else if (i == 8)
-            {
-                assertEquals(i, ELanguages.Japanese);
-            }
-            
-            else if (i == 9)
-            {
-                assertEquals(i, ELanguages.Greek);
-            }
-            
-            else if (i == 10)
-            {
-                assertEquals(i, ELanguages.Italian);
-            }
-            
-            else if (i == 11)
-            {
-                assertEquals(i, ELanguages.Portuguese);
-            }
-            
-            else if (i == 12)
-            {
-                assertEquals(i, ELanguages.Swedish);
-            }
-            
-            else if (i == 13)
-            {
-                assertEquals(i, ELanguages.Romanian);
-            }
-            
-            else if (i == 14)
-            {
-                assertEquals(i, ELanguages.Norwegian);
-            }
-            
-            else if (i == 15)
-            {
-                assertEquals(i, ELanguages.Lithuanian);
-            }
-            
-            else if (i == 16)
-            {
-                assertEquals(i, ELanguages.Hungarian);
-            }
-            
-            else if (i == 17)
-            {
-                assertEquals(i, ELanguages.Polish);
-            }
-            
-            else if (i == 18)
-            {
-                assertEquals(i, ELanguages.Dutch);
-            }
-            
-            else if (i == 19)
-            {
-                assertEquals(i, ELanguages.Danish);
-            }
-            
-            else if (i == 20)
-            {
-                assertEquals(i, ELanguages.Pashtu);
-            }
-            
-            else if (i == 21)
-            {
-                assertEquals(i, ELanguages.HaitianCreole);
-            }
-            
-            else if (i == 22)
-            {
-                assertEquals(i, ELanguages.Dari);
-            }
-            
-            else if (i == 1001)
-            {
-                assertEquals(i, ELanguages.Custom1);
-            }
-            
-            else if (i == 1002)
-            {
-                assertEquals(i, ELanguages.Custom2);
-            }
-            
-            else if (i == 1003)
-            {
-                assertEquals(i, ELanguages.Custom3);
-            }
-            
-            else if (i == 1004)
-            {
-                assertEquals(i, ELanguages.Custom4);
-            }
-            
-            else if (i == 1005)
-            {
-                assertEquals(i, ELanguages.Custom5);
-            }
-            
-            else if (i == 1006)
-            {
-                assertEquals(i, ELanguages.Custom6);
-            }
-            
-            else if (i == 1007)
-            {
-                assertEquals(i, ELanguages.Custom7);
-            }
-            
-            else if (i == 1008)
-            {
-                assertEquals(i, ELanguages.Custom8);
-            }
+
+            // ToDo
+            // No Unknown value set in the ELanguages.java file.
+            // Therefore cannot test for unknown values at this time
         }
 
     }
-
 }
