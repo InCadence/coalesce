f5e1f75a8a1d7432f8992dae6c3a68201ac17388
Jeff Hamlin <jeffrey.hamlin@gmail.com>
2014-10-07 14:59:32 -0400

    Scripts for Postgres

---
+++ src/Coalesce.Framework.Persister.PostgreSQL/src/src/main/scripts/PostGresResetDB.sql
@@ -0,0 +1,13 @@
+delete from coalesceentity;
+delete from coalescefield;
+delete from coalescerecord;
+delete from coalescerecordset;
+delete from coalesceentitytemplate;
+delete from coalescefieldbinarydata;
+delete from coalescefielddefinition;
+delete from coalescefieldhistory;
+delete from coalescelinkage;
+delete from coalescelinkagesection;
+delete from coalesceobjectmap;
+delete from coalescesection;
+
