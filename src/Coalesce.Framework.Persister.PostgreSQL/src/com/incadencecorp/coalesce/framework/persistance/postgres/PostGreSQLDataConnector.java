4e241aa208ba913dc8f21aac599a4fca60805e4c
Jeff Hamlin <jeffrey.hamlin@gmail.com>
2014-10-07 14:02:16 -0400

    PostgreSQL Persistor project

---
+++ src/Coalesce.Framework.Persister.PostgreSQL/src/com/incadencecorp/coalesce/framework/persistance/postgres/PostGreSQLDataConnector.java
@@ -0,0 +1,41 @@
+package com.incadencecorp.coalesce.framework.persistance.postgres;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.incadencecorp.coalesce.common.exceptions.CoalescePersistorException;
+import com.incadencecorp.coalesce.framework.persistance.CoalesceDataConnectorBase;
+import com.incadencecorp.coalesce.framework.persistance.ServerConn;
+
+public class PostGreSQLDataConnector extends CoalesceDataConnectorBase {
+
+    public PostGreSQLDataConnector(ServerConn settings) throws CoalescePersistorException
+    {
+        try
+        {
+            _settings = settings;
+
+            Class.forName("org.postgresql.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            throw new CoalescePersistorException("CoalesceDataConnector", e);
+        }
+    }
+
+    @Override
+    public void openConnection() throws SQLException
+    {
+        String url = "jdbc:postgresql://" + _settings.getServerNameWithPort() + "/" + _settings.getDatabase();
+
+        this._settings.setPostGres(true);
+        this._conn = DriverManager.getConnection(url, this._settings.getProperties());
+    }
+
+    @Override
+    protected String getProcedurePrefix()
+    {
+        return "call public.";
+    }
+
+}
