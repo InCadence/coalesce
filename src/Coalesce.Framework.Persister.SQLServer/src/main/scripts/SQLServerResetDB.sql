12da8807f6c9f0ebf952e3666f378160586b5caf
Jeff Hamlin <jeffrey.hamlin@gmail.com>
2014-10-09 14:29:34 -0400

    DB Scripts for SQL Server/MySQL created - to reset DB to null or get base counts during a performance test.

---
+++ src/Coalesce.Framework.Persister.SQLServer/src/main/scripts/SQLServerResetDB.sql
@@ -0,0 +1,15 @@
+use coalescedatabase;
+go
+
+delete from coalesceentity;
+delete from coalescefield;
+delete from coalescerecord;
+delete from coalescerecordset;
+delete from coalesceentitytemplate;
+delete from coalescefieldbinarydata;
+delete from coalescefielddefinition;
+delete from coalescefieldhistory;
+delete from coalescelinkage;
+delete from coalescelinkagesection;
+delete from coalesceobjectmap;
+delete from coalescesection;
