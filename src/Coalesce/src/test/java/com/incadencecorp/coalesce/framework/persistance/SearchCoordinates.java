4240289be1a8e9d10031eeec8022b55d8d44b5f0
Jing Yang <jyang@incadencecorp.com>
2016-06-28 16:22:36 -0400

    initial work for CoalesceEntitySearch UnitTest framework

---
+++ src/Coalesce/src/test/java/com/incadencecorp/coalesce/framework/persistance/SearchCoordinates.java
@@ -0,0 +1,170 @@
+package com.incadencecorp.coalesce.framework.persistance;
+
+import com.vividsolutions.jts.geom.Coordinate;
+
+/*-----------------------------------------------------------------------------'
+ Copyright 2014 - InCadence Strategic Solutions Inc., All Rights Reserved
+
+ Notwithstanding any contractor copyright notice, the Government has Unlimited
+ Rights in this work as defined by DFARS 252.227-7013 and 252.227-7014.  Use
+ of this work other than as specifically authorized by these DFARS Clauses may
+ violate Government rights in this work.
+
+ DFARS Clause reference: 252.227-7013 (a)(16) and 252.227-7014 (a)(16)
+ Unlimited Rights. The Government has the right to use, modify, reproduce,
+ perform, display, release or disclose this computer software and to have or
+ authorize others to do so.
+
+ Distribution Statement D. Distribution authorized to the Department of
+ Defense and U.S. DoD contractors only in support of U.S. DoD efforts.
+ -----------------------------------------------------------------------------*/
+/**
+ * @author Jing Yang
+ * Jun 27, 2016
+ */
+
+
+/**
+ * 
+ */
+public class SearchCoordinates {
+    
+    public static final double Re = 6371000; //earth radius in meters
+    
+    private double latitude;
+    private double longitude;
+    private double elevation;
+   
+    /**
+     * 
+     */
+    public SearchCoordinates(){
+      
+    }
+
+    /**
+     * @param Coordinate
+     */
+    public SearchCoordinates(Coordinate coord)
+    {
+        latitude = coord.y;
+        longitude = coord.x; 
+    }
+
+    public SearchCoordinates(double lat, double lon)
+    {
+        latitude = lat;
+        longitude = lon;
+        elevation = 0.0;
+    }
+    
+    // take string params (decimal degrees)
+    public SearchCoordinates(String lat, String lon, String elev)
+    {
+        latitude = Double.parseDouble(lat);
+        longitude = Double.parseDouble(lon);
+        elevation = Double.parseDouble(elev);
+    }
+    
+    // take string params (decimal degrees)
+    public SearchCoordinates(String lat, String lon)
+    {
+        latitude = Double.parseDouble(lat);
+        longitude = Double.parseDouble(lon);
+        elevation = 0.0;
+    }
+    
+    /**
+     * Constructor using an array of doubles
+     * @param location array ordered long, lat
+     */
+    public SearchCoordinates(Double location[])
+    {
+        latitude = location[1];
+        longitude = location[0];
+        elevation = 0.0;
+    }
+    
+
+
+    public double getElevation() {
+        return elevation;
+    }
+
+    public void setElevation(double elevation) {
+        this.elevation = elevation;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double distanceTo(SearchCoordinates coord)
+    {
+        double lat1 = Math.toRadians(latitude);
+        double lon1 = Math.toRadians(longitude);
+        double lat2 = Math.toRadians(coord.latitude);
+        double lon2 = Math.toRadians(coord.longitude);
+
+        double dist = ( Re * Math.acos(Math.cos(lat2) * Math.cos(lat1) * Math.cos(lon1 - lon2) + Math.sin(lat2) * Math.sin(lat1)));
+        return dist;
+    }
+
+    /**
+     * Assumes a SPHERICAL earth
+     * @param range distance to target in meters
+     * @param bearing Degrees east of north
+     * @return The location indicated by the specified range and bearing
+     */
+    public SearchCoordinates getTarget( double range, double bearing )
+    {
+        double a = range / Re;
+        double c = 0.5*Math.PI - Math.toRadians(this.latitude);
+        double beta = Math.toRadians(bearing);
+        
+        double b = Math.acos( Math.cos(a)*Math.cos(c) + Math.sin(a)*Math.sin(c)*Math.cos(beta) );
+
+        double lat = Math.toDegrees(0.5*Math.PI - b);
+        
+        double alpha = Math.asin( Math.sin(a)*Math.sin(beta) / Math.sin(b) );
+        double lon = this.longitude + Math.toDegrees(alpha);
+        
+        SearchCoordinates target = new SearchCoordinates(lat,lon);
+        return target;
+    }
+    
+    
+    /**
+     * Assumes a SPHERICAL earth
+     * @param coord
+     * @return the bearing to another coordinate as a double
+     */
+    public Double getBearingAsDouble(SearchCoordinates coord)
+    {
+        double lat1 = Math.toRadians(latitude);
+        double lon1 = Math.toRadians(longitude);
+        double lat2 = Math.toRadians(coord.latitude);
+        double lon2 = Math.toRadians(coord.longitude);
+        double dlon = lon2-lon1;
+
+        double y = Math.sin(dlon) * Math.cos(lat2);
+        double x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(dlon);
+        Double bearing = (Math.toDegrees(Math.atan2(y, x)) + 360) % 360;
+
+        return bearing;
+    }
+
+}
+
