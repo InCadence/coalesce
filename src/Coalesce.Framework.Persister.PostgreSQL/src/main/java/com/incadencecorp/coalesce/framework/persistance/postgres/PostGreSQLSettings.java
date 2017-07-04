513dc77bd2d80d93c86692e6e0abfd8d6d24dab7
Dave Boyd <dboyd@incadencecorp.com>
2017-07-03 22:30:35 -0400

    Change to use System Property as default for settings.

--- src/Coalesce.Framework.Persister.PostgreSQL/src/main/java/com/incadencecorp/coalesce/framework/persistance/postgres/PostGreSQLSettings.java
+++ src/Coalesce.Framework.Persister.PostgreSQL/src/main/java/com/incadencecorp/coalesce/framework/persistance/postgres/PostGreSQLSettings.java
@@ -20,6 +20,7 @@ package com.incadencecorp.coalesce.framework.persistance.postgres;
 import com.incadencecorp.coalesce.framework.persistance.ServerConn;
 import com.incadencecorp.unity.common.IConfigurationsConnector;
 import com.incadencecorp.unity.common.SettingsBase;
+import com.incadencecorp.unity.common.connectors.FilePropertyConnector;
 
 /**
  * Configuration properties for Postgres persister implementations.
@@ -33,7 +34,7 @@ public class PostGreSQLSettings {
     --------------------------------------------------------------------------*/
 
     private static String config_name = "postgres-config.properties";
-    private static SettingsBase settings = new SettingsBase(null);
+    private static SettingsBase settings = new SettingsBase(new FilePropertyConnector(System.getProperty("COALESCE_CONFIG_LOCATION")));
 
     /*--------------------------------------------------------------------------
     Property Names
