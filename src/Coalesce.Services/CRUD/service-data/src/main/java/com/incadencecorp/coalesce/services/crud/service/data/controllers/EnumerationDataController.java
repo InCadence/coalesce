9a937a61260544eaef5bd12a95b2a5b5cd5aa3f4
dclemenzi <dclemenzi@incadencecorp.com>
2017-08-03 06:04:43 -0400

    Modified controller to return a list instead of a map
    Removed old file

--- src/Coalesce.Services/CRUD/service-data/src/main/java/com/incadencecorp/coalesce/services/crud/service/data/controllers/EnumerationDataController.java
+++ src/Coalesce.Services/CRUD/service-data/src/main/java/com/incadencecorp/coalesce/services/crud/service/data/controllers/EnumerationDataController.java
@@ -22,7 +22,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.geotools.data.Query;
 import org.opengis.filter.Filter;
 import org.opengis.filter.FilterFactory;
 import org.opengis.filter.expression.PropertyName;
@@ -42,7 +41,9 @@ import com.incadencecorp.coalesce.services.api.search.SearchDataObjectResponse;
 import com.incadencecorp.coalesce.services.crud.api.ICrudClient;
 import com.incadencecorp.coalesce.services.crud.service.data.model.api.record.IValuesRecord;
 import com.incadencecorp.coalesce.services.crud.service.data.model.impl.coalesce.entity.EnumerationCoalesceEntity;
+import com.incadencecorp.coalesce.services.crud.service.data.model.impl.coalesce.record.MetadataCoalesceRecord;
 import com.incadencecorp.coalesce.services.crud.service.data.model.impl.pojo.entity.EnumerationPojoEntity;
+import com.incadencecorp.coalesce.services.crud.service.data.model.impl.pojo.record.MetadataPojoRecord;
 import com.incadencecorp.coalesce.services.crud.service.data.model.impl.pojo.record.ValuesPojoRecord;
 import com.incadencecorp.coalesce.services.search.api.ISearchClient;
 
@@ -69,9 +70,9 @@ public class EnumerationDataController {
     /**
      * @return a list of available enumerations.
      */
-    public Map<String, String> getEnumerations() throws RemoteException
+    public List<MetadataPojoRecord> getEnumerations() throws RemoteException
     {
-        Map<String, String> enumerations = new HashMap<String, String>();
+        List<MetadataPojoRecord> enumerations = new ArrayList<MetadataPojoRecord>();
 
         FilterFactory ff = CoalescePropertyFactory.getFilterFactory();
 
@@ -80,11 +81,10 @@ public class EnumerationDataController {
         filters.add(ff.equals(CoalescePropertyFactory.getSource(), ff.literal(EnumerationCoalesceEntity.SOURCE)));
 
         List<PropertyName> properties = new ArrayList<PropertyName>();
-        properties.add(CoalescePropertyFactory.getFieldProperty(EnumerationCoalesceEntity.RECORDSET_METADATA, "enumname"));
-
-        Query query = new Query();
-        query.setFilter(ff.and(filters));
-        query.setProperties(properties);
+        properties.add(CoalescePropertyFactory.getFieldProperty(EnumerationCoalesceEntity.RECORDSET_METADATA,
+                                                                MetadataCoalesceRecord.EMetadataFields.ENUMNAME));
+        properties.add(CoalescePropertyFactory.getFieldProperty(EnumerationCoalesceEntity.RECORDSET_METADATA,
+                                                                MetadataCoalesceRecord.EMetadataFields.DESCRIPTION));
 
         try
         {
@@ -100,7 +100,12 @@ public class EnumerationDataController {
                 {
                     for (HitType hit : result.getResult().getHits())
                     {
-                        enumerations.put(hit.getEntityKey(), hit.getValues().size() > 0 ? hit.getValues().get(0) : "");
+                        MetadataPojoRecord record = new MetadataPojoRecord();
+                        record.setKey(hit.getEntityKey());
+                        record.setEnumname(hit.getValues().get(0));
+                        record.setDescription(hit.getValues().get(1));
+
+                        enumerations.add(record);
                     }
                 }
                 else
@@ -180,9 +185,9 @@ public class EnumerationDataController {
             else
             {
                 error(String.format(CoalesceErrors.NOT_SAVED,
-                                          value.getKey(),
-                                          "enumeration",
-                                          String.format(CoalesceErrors.INVALID_ENUMERATION, value.getKey())));
+                                    value.getKey(),
+                                    "enumeration",
+                                    String.format(CoalesceErrors.INVALID_ENUMERATION, value.getKey())));
             }
         }
         catch (RemoteException | CoalesceDataFormatException e)
@@ -191,6 +196,58 @@ public class EnumerationDataController {
         }
     }
 
+// TODO Experimental code using search instead of CRUD to retrieve the values. Remove this method or replace original method.
+//    public List<ValuesPojoRecord> getEnumerationValues(String key) throws RemoteException
+//    {
+//        List<ValuesPojoRecord> values = new ArrayList<ValuesPojoRecord>();
+//
+//        List<PropertyName> properties = new ArrayList<PropertyName>();
+//        properties.add(CoalescePropertyFactory.getFieldProperty(EnumerationCoalesceEntity.RECORDSET_VALUES,
+//                                                                CoalesceObject.ATTRIBUTE_KEY));
+//        properties.add(CoalescePropertyFactory.getFieldProperty(EnumerationCoalesceEntity.RECORDSET_VALUES,
+//                                                                ValuesCoalesceRecord.EValuesFields.ORDINAL));
+//        properties.add(CoalescePropertyFactory.getFieldProperty(EnumerationCoalesceEntity.RECORDSET_VALUES,
+//                                                                ValuesCoalesceRecord.EValuesFields.VALUE));
+//        properties.add(CoalescePropertyFactory.getFieldProperty(EnumerationCoalesceEntity.RECORDSET_VALUES,
+//                                                                ValuesCoalesceRecord.EValuesFields.DESCRIPTION));
+//
+//        try
+//        {
+//            SearchDataObjectResponse results = search.search(CoalescePropertyFactory.getEntityKey(key),
+//                                                             1,
+//                                                             properties.toArray(new PropertyName[properties.size()]),
+//                                                             null,
+//                                                             true);
+//
+//            for (QueryResultsType result : results.getResult())
+//            {
+//                if (result.getResult() != null && result.getResult().getHits() != null)
+//                {
+//                    for (HitType hit : result.getResult().getHits())
+//                    {
+//                        ValuesPojoRecord value = new ValuesPojoRecord();
+//                        value.setKey(hit.getValues().get(1));
+//                        value.setOrdinal(Integer.parseInt(hit.getValues().get(1)));
+//                        value.setValue(hit.getValues().get(2));
+//                        value.setDescription(hit.getValues().get(5));
+//                    }
+//                }
+//                else
+//                {
+//                    error(String.format(CoalesceErrors.INVALID_ENUMERATION, key));
+//                }
+//            }
+//        }
+//        catch (CoalesceException e)
+//        {
+//            error(String.format(CoalesceErrors.FAILED_TASK,
+//                                this.getClass().getSimpleName(),
+//                                search.getClass().getSimpleName(),
+//                                e.getMessage()),
+//                  e);
+//        }
+//        return values;
+//    }
     public List<ValuesPojoRecord> getEnumerationValues(String key) throws RemoteException
     {
         List<ValuesPojoRecord> values = new ArrayList<ValuesPojoRecord>();
--- src/Coalesce.Services/CRUD/service-data/src/test/java/com/incadencecorp/coalesce/services/crud/service/rest/EnumerationDataControllerTest.java
+++ src/Coalesce.Services/CRUD/service-data/src/test/java/com/incadencecorp/coalesce/services/crud/service/rest/EnumerationDataControllerTest.java
@@ -18,11 +18,17 @@
 package com.incadencecorp.coalesce.services.crud.service.rest;
 
 import java.nio.file.Paths;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.sql.rowset.CachedRowSet;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.incadencecorp.coalesce.framework.CoalesceFramework;
+import com.incadencecorp.coalesce.framework.datamodel.CoalesceEntity;
+import com.incadencecorp.coalesce.framework.datamodel.CoalesceEntityTemplate;
 import com.incadencecorp.coalesce.framework.persistance.postgres.PostGreSQLPersistorExt;
 import com.incadencecorp.coalesce.framework.persistance.postgres.PostGreSQLSettings;
 import com.incadencecorp.coalesce.services.crud.service.client.CrudFrameworkClientImpl;
@@ -30,18 +36,63 @@ import com.incadencecorp.coalesce.services.crud.service.data.controllers.Enumera
 import com.incadencecorp.coalesce.services.crud.service.data.model.api.record.IValuesRecord;
 import com.incadencecorp.coalesce.services.crud.service.data.model.impl.coalesce.entity.EnumerationCoalesceEntity;
 import com.incadencecorp.coalesce.services.crud.service.data.model.impl.coalesce.record.ValuesCoalesceRecord;
+import com.incadencecorp.coalesce.services.crud.service.data.model.impl.pojo.record.MetadataPojoRecord;
 import com.incadencecorp.coalesce.services.search.service.client.SearchFrameworkClientImpl;
 import com.incadencecorp.unity.common.connectors.FilePropertyConnector;
 
 public class EnumerationDataControllerTest {
 
-    @Test
-    public void testFilterCreation() throws Exception
+    @BeforeClass
+    public static void initialize() throws Exception
     {
         FilePropertyConnector connector = new FilePropertyConnector(Paths.get("src", "test", "resources"));
         connector.setReadOnly(true);
 
         PostGreSQLSettings.setConnector(connector);
+
+        EnumerationCoalesceEntity entity = new EnumerationCoalesceEntity();
+        entity.initialize();
+        
+        PostGreSQLPersistorExt persister = new PostGreSQLPersistorExt();
+        persister.registerTemplate(CoalesceEntityTemplate.create(entity));
+}
+
+    @Test
+    public void populateEnumerations() throws Exception
+    {
+        PostGreSQLPersistorExt persister = new PostGreSQLPersistorExt();
+        
+
+        CoalesceFramework framework = new CoalesceFramework();
+        framework.setAuthoritativePersistor(persister);
+
+        SearchFrameworkClientImpl search = new SearchFrameworkClientImpl(persister);
+        CrudFrameworkClientImpl crud = new CrudFrameworkClientImpl(framework);
+
+        EnumerationDataController controller = new EnumerationDataController(crud, search);
+
+        List<String> keys = new ArrayList<String>();
+        for (MetadataPojoRecord enumeration : controller.getEnumerations())
+        {
+            keys.add(enumeration.getKey());
+        }
+
+        CoalesceEntity[] entities = persister.getEntity(keys.toArray(new String[keys.size()]));
+        
+        for (CoalesceEntity entity : entities)
+        {
+            entity.markAsDeleted();
+        }
+        
+//        persister.saveEntity(true, entities);
+        int executeUpdate = persister.executeUpdate("DELETE FROM coalesce.CoalesceEntity WHERE ObjectKey IN ('b51ce54f-316e-4b11-a3b8-de97c8c87c93','f5a52546-1ff5-47c3-8286-2285bb787384','f2d05aa3-5cf3-429d-9a49-2a5935f92fe0','b4c47906-3de2-41ac-94ac-0376fc4ec810','c9a44c60-b20a-453c-9fe8-ca4064f2e599','ac0eef49-fdde-4898-a1d8-f457194ed319','f40316df-5dbd-4d65-8b06-8f839bb8ce1f','ffefb661-02bf-460a-815b-d7a79375142c','1c6c208e-4fd4-4d15-888f-9ca66de2d58e','962d4799-df12-452f-ada3-660857b076a7','64774f64-fb87-446c-9c81-4f44b001287c','49f38a37-5e75-4441-b061-a2e7d64f003b','13ba4366-053a-4760-8c72-92f2b1779941','feb0bc03-8c31-4859-923a-c240b169fe0f','6a7849c9-0f90-4b40-92c0-eb60f2e5f5e2','270c958f-b413-4a2c-b264-70f922956b12')");
+        System.out.println(executeUpdate);
+
+    }
+
+    @Test
+    public void testFilterCreation() throws Exception
+    {
         PostGreSQLPersistorExt persister = new PostGreSQLPersistorExt();
 
         CoalesceFramework framework = new CoalesceFramework();
@@ -65,9 +116,9 @@ public class EnumerationDataControllerTest {
 
         // persister.saveEntity(false, entity);
 
-        for (Map.Entry<String, String> entry : controller.getEnumerations().entrySet())
+        for (MetadataPojoRecord entry : controller.getEnumerations())
         {
-            System.out.println(entry.getValue());
+            System.out.println(entry.getEnumname());
 
             System.out.println("\t" + controller.getEnumeration(entry.getKey()));
 
