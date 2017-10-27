a7c917470e58be45b774bf13b419fbe037bad646
dclemenzi <dclemenzi@incadencecorp.com>
2017-10-27 08:34:34 -0400

    Reverted pom changes that were preventing the react app from loading in Karaf

--- src/Coalesce.React/common-components/src/prompt.js
+++ src/Coalesce.React/common-components/src/prompt.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Popup from 'react-popup';
 
+import '../css/popup.css'
+
 /** The prompt content component */
 export default class Prompt extends React.Component {
     constructor(props) {
