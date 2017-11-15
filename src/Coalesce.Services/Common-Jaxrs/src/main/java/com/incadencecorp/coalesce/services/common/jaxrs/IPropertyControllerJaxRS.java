5d9cc363940fef8b485767df57d35ed123fe3eec
dclemenzi <dclemenzi@incadencecorp.com>
2017-11-14 22:02:11 -0500

    Implemented ability to bulk load / save settings.

--- src/Coalesce.Services/Common-Jaxrs/src/main/java/com/incadencecorp/coalesce/services/common/jaxrs/IPropertyControllerJaxRS.java
+++ src/Coalesce.Services/Common-Jaxrs/src/main/java/com/incadencecorp/coalesce/services/common/jaxrs/IPropertyControllerJaxRS.java
@@ -1,6 +1,8 @@
 package com.incadencecorp.coalesce.services.common.jaxrs;
 
 import javax.ws.rs.*;
+import java.rmi.RemoteException;
+import java.util.Map;
 
 /**
  * JaxRS annotations used on {@link com.incadencecorp.coalesce.services.common.controllers.PropertyController}
@@ -13,10 +15,21 @@ interface IPropertyControllerJaxRS {
     @GET
     @Path("/{name}")
     @Produces("application/text")
-    String getProperty(@PathParam("name") String name);
+    String getProperty(@PathParam("name") String name) throws RemoteException;
 
     @POST
     @Path("/{name}")
     @Consumes("application/text")
-    void setProperty(@PathParam("name") String name, String value);
+    void setProperty(@PathParam("name") String name, String value) throws RemoteException;
+
+    @GET
+    @Path("/")
+    @Produces("application/json")
+    Map<String, String> getProperties() throws RemoteException;
+
+    @POST
+    @Path("/")
+    @Consumes("application/json")
+    void setProperties(Map<String, String> values) throws RemoteException;
+
 }
