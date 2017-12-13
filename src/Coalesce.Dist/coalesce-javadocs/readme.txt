0e693f58f024095c71622897dff23462c0b35d32
dclemenzi <dclemenzi@incadencecorp.com>
2017-12-13 10:47:09 -0500

    Implemented Java Doc WAR
    Implemented customizable home page so each widget can be redirected to a different address as desired.
    
    Created a 'dist' profile which includes java docs and Karaf
    
    Modified property controller to allow for bulk reads of properties of interest.

---
+++ src/Coalesce.Dist/coalesce-javadocs/readme.txt
@@ -0,0 +1,1 @@
+This project pulls documentation from Coalesce.Bom. Before compiling you should run 'mvn site' on Coalesce.Bom. Eventually this should be automated.
--- src/Coalesce.Services/Common-Jaxrs/src/main/java/com/incadencecorp/coalesce/services/common/jaxrs/IPropertyControllerJaxRS.java
+++ src/Coalesce.Services/Common-Jaxrs/src/main/java/com/incadencecorp/coalesce/services/common/jaxrs/IPropertyControllerJaxRS.java
@@ -4,6 +4,7 @@ import com.incadencecorp.coalesce.services.common.controllers.PropertyController
 
 import javax.ws.rs.*;
 import java.rmi.RemoteException;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,7 +20,7 @@ interface IPropertyControllerJaxRS {
     @Produces("application/text")
     String getProperty(@PathParam("name") String name) throws RemoteException;
 
-    @POST
+    @PUT
     @Path("/{name}")
     @Consumes("application/text")
     void setProperty(@PathParam("name") String name, String value) throws RemoteException;
@@ -30,6 +31,12 @@ interface IPropertyControllerJaxRS {
     Map<String, String> getProperties() throws RemoteException;
 
     @POST
+    @Path("/")
+    @Produces("application/json")
+    @Consumes("application/json")
+    Map<String, String> getProperties(String[] names) throws RemoteException;
+
+    @PUT
     @Path("/")
     @Consumes("application/json")
     void setProperties(Map<String, String> values) throws RemoteException;
