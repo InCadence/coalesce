752b19d4a12b80a94a1dfee09f5f8652f4b592c8
dclemenzi <dclemenzi@incadencecorp.com>
2018-01-04 13:56:38 -0500

    Added on to the deploy scripted and modified it to accept a server as a parameter.
    Added javadocs to the installed features of the karaf dist.
    
    Resolved warnings with the blueprint controller.
    
    Resolved issue on AWS instance where the rest blueprint would not render due to a class not found error. Modified the code to be based off the interface instead of the impl.

--- src/Coalesce.Dist/coalesce-karaf-dist/deploy.sh
+++ src/Coalesce.Dist/coalesce-karaf-dist/deploy.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 
-SERVER=bdpgeoserver.bdpdev.incadencecorp.com
-DIR=$1
+#bdpgeoserver.bdpdev.incadencecorp.com
+#oeservices
 
-echo ${DIR}
+SERVER=$1
+DIR=$2
 
-scp -i ~/BDP_key.pem target/coalesce-karaf-dist-0.0.25-SNAPSHOT.tar.gz root@${SERVER}:~/.
-ssh -i  ~/BDP_key.pem  root@${SERVER} mkdir ${DIR}
-ssh -i  ~/BDP_key.pem  root@${SERVER} tar -xf coalesce-karaf-dist-0.0.25-SNAPSHOT.tar.gz -C ${DIR}/.
+echo 
+echo root@${SERVER}:${DIR}
+
+while true; do
+
+	echo
+	echo "Menu:"
+	echo "1. Deploy (Auto Starts)"
+	echo "2. Start"
+	echo "3. Stop"
+	echo "4. Remove"
+	echo "5. Exit"
+
+	read -p ": " input
+
+	case $input in 
+
+		1) 
+			scp -i ~/BDP_key.pem target/coalesce-karaf-dist-0.0.25-SNAPSHOT.tar.gz root@${SERVER}:~/.
+			ssh -i  ~/BDP_key.pem root@${SERVER} mkdir ${DIR}
+			ssh -i  ~/BDP_key.pem root@${SERVER} tar -xf coalesce-karaf-dist-0.0.25-SNAPSHOT.tar.gz -C ${DIR}/.
+			ssh -i  ~/BDP_key.pem root@${SERVER} ${DIR}/bin/start;;
+		2) ssh -i  ~/BDP_key.pem root@${SERVER} ${DIR}/bin/start;;
+		3) ssh -i  ~/BDP_key.pem root@${SERVER} ${DIR}/bin/stop;;
+		4) ssh -i  ~/BDP_key.pem root@${SERVER} rm -rf ${DIR};;
+		5) exit 0;;
+		*) echo Invalid Input
+	esac 
+
+
+done
