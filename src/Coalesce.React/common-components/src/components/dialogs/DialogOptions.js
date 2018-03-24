db2e06a88777440537fe553d54f5b9ce326b341d
dclemenzi <dclemenzi@incadencecorp.com>
2018-03-24 18:08:33 -0400

    Implemented propertyController.js and common dialogs within common-components.
    Switch JSON editor over to material-ui and added a prompt to select which configuration file to edit.

---
+++ src/Coalesce.React/common-components/src/components/dialogs/DialogOptions.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import {List, ListItem } from 'material-ui/List';
+
+export class DialogOptions extends React.Component {
+
+  renderItem(item) {
+    return (
+      <ListItem
+        key={item.key}
+        id={item.key}
+        primaryText={item.name}
+        onClick={item.onClick}
+      />
+    )
+  }
+
+  render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onClick={this.props.onClose}
+      />,
+    ];
+
+    return (
+      <Dialog
+        title={this.props.title}
+        actions={actions}
+        modal={false}
+        open={this.props.open}
+        onRequestClose={this.props.onClose}
+      >
+        <List>
+          {this.props.options.map((item) => {return this.renderItem(item);})}
+        </List>
+      </Dialog>
+    )
+  }
+
+}
