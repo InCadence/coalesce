a276c5f220ed2bc30da899bba86e4fcaa19155ec
dclemenzi <dclemenzi@incadencecorp.com>
2018-04-24 11:43:29 -0400

    Commands to run Elastic Search. Not sure how to pass in the -E arguments through the docker-compose file. Until that is resolved these commands can be ran manual.

---
+++ src/Coalesce.Framework.Persister.Elasticsearch/src/test/resources/dockerrun.sh
@@ -0,0 +1,2 @@
+docker run --name rbcf-elasticsearch -p 9200:9200 -p 9300:9300 -d elasticsearch:5.4.0 -E "http.host=0.0.0.0" -E "transport.host=0.0.0.0" -E "cluster.name=coalesce"
+docker run --name rbcf-kibana --link rbcf-elasticsearch:elasticsearch -p 5601:5601 -d kibana:5.4.0
