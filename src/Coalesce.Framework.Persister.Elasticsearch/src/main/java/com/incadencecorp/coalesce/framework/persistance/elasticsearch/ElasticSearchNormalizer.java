c45f0e9ec9f7247d048c256d3914b03e3f7feb72
dclemenzi <dclemenzi@incadencecorp.com>
2018-05-18 11:38:32 -0400

    Missed file

---
+++ src/Coalesce.Framework.Persister.Elasticsearch/src/main/java/com/incadencecorp/coalesce/framework/persistance/elasticsearch/ElasticSearchNormalizer.java
@@ -0,0 +1,39 @@
+/*
+ *  Copyright 2017 - InCadence Strategic Solutions Inc., All Rights Reserved
+ *
+ *  Notwithstanding any contractor copyright notice, the Government has Unlimited
+ *  Rights in this work as defined by DFARS 252.227-7013 and 252.227-7014.  Use
+ *  of this work other than as specifically authorized by these DFARS Clauses may
+ *  violate Government rights in this work.
+ *
+ *  DFARS Clause reference: 252.227-7013 (a)(16) and 252.227-7014 (a)(16)
+ *  Unlimited Rights. The Government has the right to use, modify, reproduce,
+ *  perform, display, release or disclose this computer software and to have or
+ *  authorize others to do so.
+ *
+ *  Distribution Statement D. Distribution authorized to the Department of
+ *  Defense and U.S. DoD contractors only in support of U.S. DoD efforts.
+ *
+ */
+
+package com.incadencecorp.coalesce.framework.persistance.elasticsearch;
+
+import com.incadencecorp.coalesce.api.ICoalesceNormalizer;
+import com.incadencecorp.coalesce.framework.DefaultNormalizer;
+
+/**
+ * This normalizer is full use by ElasticSearch to swap the separating character.
+ *
+ * @author Derek Clemenzi
+ */
+public class ElasticSearchNormalizer extends DefaultNormalizer implements ICoalesceNormalizer {
+
+    private static final String SEPERATOR = "_";
+
+    @Override
+    public String normalize(final String recordsetname, final String fieldname)
+    {
+        return normalize(recordsetname) + SEPERATOR + normalize(fieldname);
+    }
+
+}
