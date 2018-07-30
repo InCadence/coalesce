5581b3e8e9a4642d213df6683e9ea4826bf9d07e
dclemenzi <dclemenzi@incadencecorp.com>
2018-07-30 06:34:31 -0400

    Resolved copy paste error.

--- src/Coalesce.React/settings/src/index.js
+++ src/Coalesce.React/settings/src/index.js
@@ -9,16 +9,16 @@ var pjson = require('../package.json');
 document.title = pjson.title;
 
 function loadApplication(theme) {
+  loadAllProperties().then((data) => {
   ReactDOM.render(
-    <App icon={pjson.icon} title={pjson.title} theme={createMuiTheme(theme)}/>,
+    <App data={data} icon={pjson.icon} title={pjson.title} theme={createMuiTheme(theme)}/>,
     document.getElementById('main')
   );
+})
 }
 
 loadJSON('theme').then((theme) => {
-  loadAllProperties().then((data) => {
     loadApplication(createMuiTheme(theme));
-  })
 }).catch((err) => {
   console.log("Loading Theme: " + err);
   loadApplication(createMuiTheme({}));
