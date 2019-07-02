511a467cbfa2f075bb66846f16cdcbd8c907782b
Scott Orr <sorr@incadencecorp.com>
2019-07-02 11:12:00 -0400

    Fixed automatic versioning for pyCoalesce, including workable code for standalone deployment.

--- src/pyCoalesce/README.md
+++ src/pyCoalesce/README.md
@@ -1,10 +1,32 @@
-The pyCoalesce package itself is found in the pyCoalesce directory.  The 
-other directories contain documentation (and source for creating
-documentation), a script for generating classes from the the Coalesce entity
-XSD, tests, and build tools.
+Copyright 2018-9, InCadence Strategic Solutions
+
+
+The package pyCoalesce is a wrapper for the Coalesce database abstraction layer,
+which provides a common API for interacting with multiple databases.
+
+A standalone version of pyCoalesce can be found at:
+https://pypi.org/project/pyCoalesce/
+
+To install pyCoalesce via pip, simply "pip install pyCoalesce".
 
 The "lxml" package is recommended for manipulating XSD-based classes, but
 it's not absolutely required.
+
+
+The full Coalesce project can be found at:
+https://github.com/InCadence/coalesce
+
+The pyCoalesce version number corresponds to the Coalesce version number, but 
+is modified to meet PEP 440 and PyPi rules:  release version numbers are 
+identical, but for snapshot versions, "snapshot" is replaced with "preX", where
+"X" is a sequential number incremented whenever a pyCoalesce snapshot is
+distributed.
+
+The pyCoalesce package is found in the "pyCoalesce" directory of the Coalesce
+repository.  In addition to the pyCoalesce source files, this directory and its
+sub-directories contain documentation (and source for creating documentation), a
+script for generating classes from the the Coalesce entity XSD, tests, and 
+distribution tools.
 
 The "unit tests" are not true unit tests:  later tests rely on the entities
 created in earlier tests, thereby avoiding either repeated creation and 
@@ -12,12 +34,7 @@ deletion of entities, or testing against a mocked-up API.  (The latter would
 be unnecessarily time-consuming to create, and wouldn't catch bugs in the API
 itself, which was still a consideration when this wrapper was created.)
 
-The full Coalesce project can be found at:  https://github.com/InCadence/coalesce
-
-A standalone version of pyCoalesce can be found at:  https://pypi.org/project/pyCoalesce/
-
-To install pyCoalesce via pip, simply "pip install pyCoalesce".
-
-To recreate the distribution files, run the following command from this directory (typical options have been included):
+To recreate the distribution files, run the following command from this 
+directory (typical options have been included):
 
 python setup.py sdist bdist_wheel
--- src/pyCoalesce/setup.py
+++ src/pyCoalesce/setup.py
@@ -1,36 +1,203 @@
+"""
+Copyright 2018-9, InCadence Strategic Solutions
+
+"""
+
+
+import re
+from shutil import copyfile
 import setuptools
 
-from version import Coalesce_version, author
 
+# This constant can be changed to a custom label.
+LOCAL_VERSION_LABEL = "local"
 
+# Read in the README file.
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-# PyPi won't accept "local" version identifiers, so we have to replace the
-# "+snapshot" version identifier segment.
-if "+" in Coalesce_version:
+# Read in the last pyCoalesce version number.
+try:
+    with open("pyCoalesce/version.txt", "r") as version_file:
+        last_version = version_file.read()
+except:
+    last_version = "0.0.0.dev0"
+
+# Try to read a version number from the larger Coalesce package, and then
+# process it to create a PyPi-compliant version number.  This won't work in
+# the sdist (tar.gz) version downloaded from PyPi--indeed, the "version.py"
+# module won't even be present.
+
+try:
+
+    # We do this here because the module in question won't exist in the
+    # standalone distribution.  This is the actual line caught in the
+    # "except".  The rest of this block is included here, rather than in
+    # an "else" block, for readability.
+    from version import Coalesce_version
+
+    # Parse the Coalesce version.
+    version_match = \
+        re.match("([0-9]+\.[0-9]+\.[0-9]+)([^a-zA-Z0-9\.]?)([a-zA-Z0-9\.]*)",
+                 Coalesce_version)
+    main_version = version_match.group(1)
+    suffix = version_match.group(3).lower()
+
+    # Check the previous pyCoalesce version.
+    last_version_list = last_version.split(".dev")
+    last_main_version = last_version_list[0]
     try:
-        with open("last_snapshot.txt", "r") as ss:
-            last_snap_version = ss.read()
-    except:
-        last_snap_version = "0.0.0.post0"
-    main_version = Coalesce_version.split("+")[0]
-    last_snap_version_list = last_snap_version.split(".post")
-    last_snap_main_version = last_snap_version_list[0]
-    if main_version == last_snap_main_version:
-        snap_number = int(last_snap_version_list[1]) + 1
+        last_dev_number = int(last_version_list[1])
+    except IndexError:
+        last_dev_number = None
+
+    # Transform the Coalesce version number into a pyCoalesce version
+    # number compliant with PEP 440 and PyPi.
+
+    # If the current Coalesce version is a snapshot:
+    if suffix == "snapshot":
+
+        # If the last version of pyCoalesce was a dev version based on the
+        # current main version of Coalesce, in which case this must be the
+        # second or later dev version of the current main version:
+        if main_version == last_main_version:
+            dev_number = last_dev_number + 1
+
+        # If the last version of pyCoalesce was a dev version based on the
+        # the previous main version of Coalesce (or a snapshot thereof), in
+        # which case this must be the first dev version of the current
+        # main version:
+        else:
+            dev_number = 1
+
+    # If the current version of Coalesce is a release:
     else:
-        snap_number = 1
-    version = main_version + ".post" + str(snap_number)
-    with open("last_snapshot.txt", "w") as ss:
-        ss.write(version)
-else:
-    version = Coalesce_version
+
+        # Which has the latest main version, the current Coalesce version
+        # of the last pyCoalesce version?  Usually, the current Coalesce
+        # version will be the same as or later than the last pyCoalesce
+        # version, but it's possible for the pyCoalesce main version to be
+        # later in rare cases, when two or more pyCoalesce versions are
+        # created before any Coalesce snapshot has been created.
+
+        # Parse the version number.
+        main_version_list = main_version.split(".")
+        last_main_version_list = last_main_version.split(".")
+        major = int(main_version_list[0])
+        minor = int(main_version_list[1])
+        patch = int(main_version_list[2])
+        last_major = int(last_main_version_list[0])
+        last_minor = int(last_main_version_list[1])
+        last_patch = int(last_main_version_list[2])
+
+        # Compare major, minor, and patch numbers.
+
+        if major > last_major:
+            latest = "Coalesce"
+
+        elif last_major > major:
+            latest = "pyCoalesce"
+
+        else:
+
+            if minor > last_minor:
+                latest = "Coalesce"
+
+            elif last_minor > minor:
+                latest = "pyCoalesce"
+
+            else:
+
+                if patch > last_patch:
+                    latest = "Coalesce"
+
+                elif last_patch > patch:
+                    latest = "pyCoalesce"
+
+                else:
+                    latest= "both"
+
+        # If the Coalesce version number is higher (meaning the previous
+        # version of pyCoalesce was part of the previous Coalesce release),
+        # in which case this version of pyCoalesce must be part of the
+        # larger Coalesce release:
+        if latest == "Coalesce":
+            dev_number = None
+
+        # If the Coalesce and last pyCoalesce version numbers are the same:
+        elif latest == "both":
+
+            # If the last pyCoalesce version was a dev version of this
+            # Coalesce release, in which case this version of pyCoalesce
+            # must be part of the larger Coalesce release:
+            if last_dev_number:
+                pre_number = None
+
+            # If the last pyCoalesce version was part of the current
+            # pyCoalesce release (a rare occurrence that can only happen
+            # when a pyCoalesce version is created before a snapshot has
+            # been generated for a given Coalesce version):
+            else:
+
+                # We're going to increment the main version number here.
+                # In rare cases, this could produce an inaccurate version
+                # number:  if, #1, we're producing a pyCoalesce dev version
+                # before any Coalesce snapshot has created and, #2, the
+                # next release of Coalesce will be a new major or minor
+                # version.
+                new_patch = patch + 1
+                main_version = ".".join([str(major), str(minor),
+                                         str(new_patch)])
+
+                dev_number = 1
+
+        # If the last pyCoalesce version number is higher (an extremely
+        # rare case that can occur if two or more pyCoalesce versions are
+        # created before the first Coalesce snaphost has been generated for
+        # given main version):
+        else:
+            main_version = last_main_version
+            dev_number = last_dev_number + 1
+
+    # Form the pyCoalesce version number:
+    if dev_number:
+        version = main_version + ".dev" + str(dev_number)
+    else:
+        version = main_version
+
+# If we can't read in a Coalesce version number, this must be a local
+# installation, and therefore we'll assign a local version number.  The
+# following section can be modified to create a customized pattern for
+# local version numbers.
+
+except ImportError:
+
+    # To obtain the distributed version number, split off the local suffix.
+    version_list = Coalesce_version.split("+" + LOCAL_VERSION_LABEL)
+    distributed_version = version_list[0]
+
+    # Find the last local number, if any.
+    last_local_number = version_list[1]
+    if last_local_number:
+        local_number = int(last_local_number) + 1
+    else:
+        local_number = 1
+
+    # Construct a new local version number.
+    version = distributed_version + "+" + LOCAL_VERSION_LABEL + \
+              str(local_number)
+
+with open("pyCoalesce/version.txt", "w") as version_file:
+    version_file.write(version)
+
+# Copy the Coalesce license file; this is used only by Twine (and really
+# shouldn't be necessary, since PyPi doesn't make sure of it).
+copyfile("../../LICENSE", "LICENSE")
 
 setuptools.setup(
     name = "pyCoalesce",
     version = version,
-    author = author,
+    author = u"Dhruva Venkat, Scott Orr",
     author_email = "sorr@incadencecorp.com",
     description = "A python wrapper for coalesce objects",
     license = "Apache License 2.0",
